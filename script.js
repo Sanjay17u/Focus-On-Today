960bc4c1a6c3f3d4528c86995f9958a99aabb96f
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2025-02-08 19:31:06 +0530

    input fields logic implemented

--- script.js
+++ script.js
@@ -1,23 +1,33 @@
-const inputs = document.querySelectorAll('input')
-let count = 0
+const inputs = document.querySelectorAll('input');
+const inputsArray = Array.from(inputs);
+const checkBox = document.querySelectorAll('.input__checkBox');
 
-for(let x = 0; x < inputs.length; x++) {
-    inputs[x].addEventListener('input', (e) => {
-        // console.log(e)
-        if(e.target.value.trim() != '') {
-            console.log(true)
-            count++
-            console.log(count)
-        } else {
-            console.log(false)
-            count--
-            console.log(count)
+
+checkBox.forEach((checkBox) => {
+    checkBox.addEventListener('click', () => {
+        
+        const inputFields = inputsArray.every((input) => {
+            console.log(input.value)
+            return input.value
+        })
+        
+        if(inputFields) {
+            console.log('checked')
+            checkBox.nextElementSibling.classList.toggle('Green__Text')
+            checkBox.classList.toggle('Green__Tick')
+
+            const existingImg = checkBox.querySelector('.right__check')
+
+            if(!existingImg) {
+                const imgElement = document.createElement('img')
+                imgElement.classList.add('right__check')
+                imgElement.src = './images/Vector 1.svg'
+
+                checkBox.append(imgElement)
+            } else {
+                checkBox.removeChild(existingImg)
+            }
         }
 
-        if(count === inputs.length) {
-            console.log("All inputs are filled: true")
-        } else {
-            console.log("All inputs are filled: false")
-        }
     })
-}
+})
