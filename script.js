e40a6a4c994a24765aa9e6f98deb8dd0ececeb02
Sanjay Solanki <sanjay.solanki.17.om@gmail.com>
2025-02-09 09:35:03 +0530

    Code Revised.

--- script.js
+++ script.js
@@ -3,18 +3,24 @@ const inputsArray = Array.from(inputs);
 const checkBox = document.querySelectorAll('.input__checkBox');
 
 
-checkBox.forEach((checkBox) => {
+checkBox.forEach((checkBox, index) => {
     checkBox.addEventListener('click', () => {
-        
+
         const inputFields = inputsArray.every((input) => {
             console.log(input.value)
-            return input.value
+            return input.value.trim() !== ''
         })
-        
+
         if(inputFields) {
             console.log('checked')
             checkBox.nextElementSibling.classList.toggle('Green__Text')
             checkBox.classList.toggle('Green__Tick')
+
+            if(checkBox.classList.contains('Green__Tick')) {
+                inputs[index].disabled = true
+            } else {
+                inputs[index].disabled = false
+            }
 
             const existingImg = checkBox.querySelector('.right__check')
 
